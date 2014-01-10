b559458f074331efecd2178a9f9dd27e01431442
Xudong Huang <xudongh@juniper.net>
2014-01-10 15:27:48 +0800

    update file structure format

--- diagd/platform/handlers/src/dg_aux_cmd.c
+++ diagd/platform/handlers/src/dg_aux_cmd.c
@@ -6,37 +6,46 @@
 
 ====================================================================================================
 
-Revision History:
-                            Modification     Tracking
-Author                          Date          Number     Description of Changes
--------------------------   ------------    ----------   -------------------------------------------
-Xudong Huang    - xudongh    2013/12/11     xxxxx-0000   Creation
-Xudong Huang    - xudongh    2013/12/20     xxxxx-0003   Enable aux engine
-
 ====================================================================================================
-                                            INCLUDE FILES
+                                           INCLUDE FILES
 ==================================================================================================*/
 #include "dg_handler_inc.h"
 #include "dg_aux_engine.h"
 
+
+/** @addtogroup common_command_handlers
+@{
+*/
+
+/** @addtogroup AUX
+@{
+
+@par
+<b>AUX - 0xDEAD</b>
+
+@par
+Handles AUX engine diag command
+engine
+*/
+
 /*==================================================================================================
-                                           LOCAL CONSTANTS
+                                          LOCAL CONSTANTS
 ==================================================================================================*/
 
 /*==================================================================================================
-                                            LOCAL MACROS
+                                           LOCAL MACROS
 ==================================================================================================*/
 
 /*==================================================================================================
-                             LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+                            LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
 ==================================================================================================*/
 
 /*==================================================================================================
-                                      LOCAL FUNCTION PROTOTYPES
+                                     LOCAL FUNCTION PROTOTYPES
 ==================================================================================================*/
 
 /*==================================================================================================
-                                          GLOBAL VARIABLES
+                                         GLOBAL VARIABLES
 ==================================================================================================*/
 
 /*==================================================================================================
@@ -44,7 +53,7 @@ Xudong Huang    - xudongh    2013/12/20     xxxxx-0003   Enable aux engine
 ==================================================================================================*/
 
 /*==================================================================================================
-                                          GLOBAL FUNCTIONS
+                                         GLOBAL FUNCTIONS
 ==================================================================================================*/
 
 /*=============================================================================================*//**
@@ -52,16 +61,16 @@ Xudong Huang    - xudongh    2013/12/20     xxxxx-0003   Enable aux engine
 
 @param[in] req - DIAG request
 *//*==============================================================================================*/
-void DG_AUX_CMD_handler_main(DG_DEFS_DIAG_REQ_T *req)
+void DG_AUX_CMD_handler_main(DG_DEFS_DIAG_REQ_T* req)
 {
     /* Create response builder */
-    DG_DEFS_DIAG_RSP_BUILDER_T *rsp;   
-    rsp = DG_ENGINE_UTIL_rsp_init(); 
+    DG_DEFS_DIAG_RSP_BUILDER_T* rsp;
+    rsp = DG_ENGINE_UTIL_rsp_init();
 
-#if DG_CFG_AUX_NUM==0
+#if DG_CFG_AUX_NUM == 0
     /* If no modem existing, return error opcode */
     DG_ENGINE_UTIL_rsp_set_code(rsp, DG_RSP_CODE_PAR_ERR_OPCODE);
-#elif DG_CFG_AUX_NUM==1
+#elif DG_CFG_AUX_NUM == 1
     DG_AUX_ENGINE_handle_aux_cmd(DG_AUX_ENGINE_AUX_ID_AUX1, req, rsp);
 #else
     /* Use an arbitary opcode value to determine which aux engine to forward the request to */
@@ -76,12 +85,15 @@ void DG_AUX_CMD_handler_main(DG_DEFS_DIAG_REQ_T *req)
 #endif
 
     DG_ENGINE_UTIL_rsp_send(rsp, req);
-    
+
     /* Free response builder */
     DG_ENGINE_UTIL_rsp_free(rsp);
-    
 }
 
 /*==================================================================================================
                                           LOCAL FUNCTIONS
 ==================================================================================================*/
+
+/** @} */
+/** @} */
+
