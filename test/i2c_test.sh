18fdb4a7cca9a50544b8698d104f42c00e01c7c3
Xudong Huang <xudongh@juniper.net>
2014-01-28 14:46:30 +0800

    Update I2C test script

--- test/i2c_test.sh
+++ test/i2c_test.sh
@@ -5,6 +5,20 @@ CUR_PATH=$(dirname $0)
 SEC_NAME="I2C Test"
 OPCODE=0010
 
+RD_ACTION=00
+WR_ACTION=01
+
+BUS=00
+ADDRESS=50
+RD_OFFSET=00
+RD_LEN=ff
+
+WR_OFFSET=fd
+WR_LEN=02
+WR_DATA1=0102
+WR_DATA2=ffff
+
+
 echo "#-----------------------$SEC_NAME-----------------------"
 
 #Define array for command and description: following Diag and DVT Spec
@@ -12,20 +26,25 @@ echo "#-----------------------$SEC_NAME-----------------------"
 #command array
 array_command[0]=01000102
 array_command[1]=0ffd00000000
-array_command[2]=$OPCODE""0000510210
-array_command[3]=$OPCODE""01005302020103
-array_command[4]=0ffd00000001
+array_command[2]="$OPCODE""$RD_ACTION""$BUS""$ADDRESS""$RD_OFFSET""$RD_LEN"
+array_command[3]="$OPCODE""$WR_ACTION""$BUS""$ADDRESS""$WR_OFFSET""$WR_LEN""$WR_DATA1"
+array_command[4]="$OPCODE""$RD_ACTION""$BUS""$ADDRESS""$RD_OFFSET""$RD_LEN"
+array_command[5]="$OPCODE""$WR_ACTION""$BUS""$ADDRESS""$WR_OFFSET""$WR_LEN""$WR_DATA2"
+array_command[6]="$OPCODE""$RD_ACTION""$BUS""$ADDRESS""$RD_OFFSET""$RD_LEN"
+array_command[7]=0ffd00000001
 
 #command description array, need match with command array above.
 array_des[0]="Suspend the DUT"
 array_des[1]="Enable Data Dump"
-array_des[2]="Read I2C Bus"
-array_des[3]="Write I2C Bus"
-array_des[4]="Disable Data Dump"
+array_des[2]="Read I2C Bus=$BUS Address=$ADDRESS Offset=$RD_OFFSET Len=$RD_LEN"
+array_des[3]="Write I2C Bus=$BUS Address=$ADDRESS Offset=$WR_OFFSET Len=$WR_LEN Data=$WR_DATA1"
+array_des[4]="Read I2C Bus=$BUS Address=$ADDRESS Offset=$RD_OFFSET Len=$RD_LEN"
+array_des[5]="Write I2C Bus=$BUS Address=$ADDRESS Offset=$WR_OFFSET Len=$WR_LEN Data=$WR_DATA2"
+array_des[6]="Read I2C Bus=$BUS Address=$ADDRESS Offset=$RD_OFFSET Len=$RD_LEN"
+array_des[7]="Disable Data Dump"
 
-array_command_fail[0]=$OPCODE""0001510210
-
-array_des_fail[0]="Suspend invalid Bus"
+array_command_fail[0]=$OPCODE""0008510210
+array_des_fail[0]="Read invalid Bus"
 
 . $CUR_PATH/diag_verify.sh
 
