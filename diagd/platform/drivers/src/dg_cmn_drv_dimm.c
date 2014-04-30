b00fed8a4bd910eea4621a11bafc45b31ab2b46f
Xudong Huang <xudongh@juniper.net>
2014-04-30 11:09:47 +0800

    implement dimm dump for es3

--- diagd/platform/drivers/src/dg_cmn_drv_dimm.c
+++ diagd/platform/drivers/src/dg_cmn_drv_dimm.c
@@ -12,6 +12,7 @@
 #include "dg_handler_inc.h"
 #include "dg_drv_util.h"
 #include "dg_cmn_drv_dimm.h"
+#include "dg_cmn_drv_i2c.h"
 
 
 /** @addtogroup dg_common_drivers
@@ -62,20 +63,28 @@ BOOL DG_CMN_DRV_DIMM_get_spd(DG_CMN_DRV_DIMM_SLOT_T slot, DG_CMN_DRV_DIMM_SPD_T*
 {
     BOOL ret = FALSE;
 
+    DG_CMN_DRV_I2C_ADDR_T addr;
+    DG_CMN_DRV_I2C_SIZE_T size = 128; /* one time read size */
+
     switch (slot)
     {
     case DG_CMN_DRV_DIMM_SLOT_0:
     case DG_CMN_DRV_DIMM_SLOT_1:
     case DG_CMN_DRV_DIMM_SLOT_2:
     case DG_CMN_DRV_DIMM_SLOT_3:
-        memset(spd->value, slot, sizeof(spd->value));
-        DG_DBG_TRACE("Get DIMM SPD from slot %d", slot);
-        DG_DBG_DUMP(spd->value, sizeof(spd->value));
-        ret = TRUE;
+        addr = 0x50 + slot;
+        if (DG_CMN_DRV_I2C_read_bus(DG_CMN_DRV_I2C_PCH_SMB, addr, 0, size, spd->value) &&
+            DG_CMN_DRV_I2C_read_bus(DG_CMN_DRV_I2C_PCH_SMB, addr, size,
+                                    sizeof(spd->value) - size, spd->value + size))
+        {
+            DG_DBG_TRACE("Get DIMM SPD from slot %d", slot);
+            DG_DBG_DUMP(spd->value, sizeof(spd->value));
+            ret = TRUE;
+        }
         break;
 
     default:
-        DG_DRV_UTIL_set_error_string("Read DIMM SPD failed: slot=0x%02x", slot);
+        DG_DRV_UTIL_set_error_string("Invalid SPD, slot=0x%02x", slot);
         break;
     }
 
