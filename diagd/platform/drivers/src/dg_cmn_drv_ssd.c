738f0e62419c54a4b72f376030d0faaf3c2af4f8
Levy Wang <levywang@juniper.net>
2014-05-09 18:01:46 +0800

    implement ssd for es3
    Change-Id: I4e6bc85f66feb8885b2d5fc3da15f5abff5c6075
    Reviewed-on: http://10.208.24.69/229
    Reviewed-by: Xudong Huang <xudongh@juniper.net>

--- diagd/platform/drivers/src/dg_cmn_drv_ssd.c
+++ diagd/platform/drivers/src/dg_cmn_drv_ssd.c
@@ -13,6 +13,7 @@
 #include "dg_handler_inc.h"
 #include "dg_drv_util.h"
 #include "dg_cmn_drv_ssd.h"
+#include "dg_cmn_drv_cpld.h"
 
 
 /** @addtogroup dg_common_drivers
@@ -49,6 +50,7 @@ implementation of the SSD driver
 /*==================================================================================================
                                           LOCAL VARIABLES
 ==================================================================================================*/
+static char* dg_cmn_drv_device[] = { "/dev/sda", "/dev/sdb" };
 
 /*==================================================================================================
                                          GLOBAL FUNCTIONS
@@ -62,15 +64,28 @@ implementation of the SSD driver
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_SSD_get_status(DG_CMN_DRV_SSD_ID_T ssd, DG_CMN_DRV_SSD_STATUS_T* status)
 {
-    BOOL ret = FALSE;
+    BOOL                    ret = FALSE;
+    DG_CMN_DRV_CPLD_VALUE_T value;
 
     switch (ssd)
     {
     case DG_CMN_DRV_SSD_OS:
-    case DG_CMN_DRV_SSD_LOG:
         *status = DG_CMN_DRV_SSD_PRESENCE_MASK;
         DG_DBG_TRACE("Get SSD status 0x%02x", *status);
         ret = TRUE;
+        break;
+
+    case DG_CMN_DRV_SSD_LOG:
+        if (!DG_CMN_DRV_CPLD_get(DG_CMN_DRV_CPLD_CB, 0x18, &value))
+        {
+            DG_DBG_ERROR("can't get CPLD SSD status register");
+        }
+        else
+        {
+            *status = value & (DG_CMN_DRV_SSD_PRESENCE_MASK | DG_CMN_DRV_SSD_POW_FAIL_MASK);
+            DG_DBG_TRACE("Get SSD status 0x%02x", *status);
+            ret = TRUE;
+        }
         break;
 
     default:
@@ -88,20 +103,31 @@ BOOL DG_CMN_DRV_SSD_get_status(DG_CMN_DRV_SSD_ID_T ssd, DG_CMN_DRV_SSD_STATUS_T*
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_SSD_get_model(DG_CMN_DRV_SSD_ID_T ssd, char** str)
 {
-    BOOL   ret     = FALSE;
-    char*  model[] = { "SATA SSD", "ST500DM002-1BD142" };
-    UINT32 len;
+    BOOL  ret = FALSE;
+    char  shell_cmd[128];
+    char* p_out = NULL;
 
     switch (ssd)
     {
     case DG_CMN_DRV_SSD_OS:
     case DG_CMN_DRV_SSD_LOG:
-        len = strlen(model[ssd]) + 1;
-        if ((*str = malloc(len)))
+        sprintf(shell_cmd, "hdparm -I %s |grep Model |cut -d: -f2 |sed -e 's/^ *//g' -e's/ *$//g'",
+                dg_cmn_drv_device[ssd]);
+        if (DG_DRV_UTIL_system(shell_cmd, &p_out))
         {
-            strncpy(*str, model[ssd], len);
-            DG_DBG_TRACE("Get SSD model %s", *str);
-            ret = TRUE;
+            if (p_out != NULL)
+            {
+                /* remove the tail '\n' */
+                int last = strlen(p_out) - 1;
+                if (p_out[last] == '\n')
+                {
+                    p_out[last] = '\0';
+                }
+                DG_DBG_TRACE("Get SSD model %s", p_out);
+                ret = TRUE;
+            }
+
+            *str = p_out;
         }
         break;
 
@@ -120,22 +146,32 @@ BOOL DG_CMN_DRV_SSD_get_model(DG_CMN_DRV_SSD_ID_T ssd, char** str)
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_SSD_get_serial(DG_CMN_DRV_SSD_ID_T ssd, char** str)
 {
-    BOOL   ret      = FALSE;
-    char*  serial[] = { "W3T27KY4", "FED10738028500038456" };
-    UINT32 len;
+    BOOL  ret = FALSE;
+    char  shell_cmd[128];
+    char* p_out = NULL;
 
     switch (ssd)
     {
     case DG_CMN_DRV_SSD_OS:
     case DG_CMN_DRV_SSD_LOG:
-        len = strlen(serial[ssd]) + 1;
-        if ((*str = malloc(len)))
+        sprintf(shell_cmd, "hdparm -I %s |grep 'Serial Number' |cut -d: -f2 |sed -e 's/^ *//g'"
+                " -e's/ *$//g'", dg_cmn_drv_device[ssd]);
+        if (DG_DRV_UTIL_system(shell_cmd, &p_out))
         {
-            strncpy(*str, serial[ssd], len);
-            DG_DBG_TRACE("Get SSD serial %s", *str);
-            ret = TRUE;
+            if (p_out != NULL)
+            {
+                /* remove the tail '\n' */
+                int last = strlen(p_out) - 1;
+                if (p_out[last] == '\n')
+                {
+                    p_out[last] = '\0';
+                }
+                DG_DBG_TRACE("Get SSD serial %s", p_out);
+                ret = TRUE;
+            }
+
+            *str = p_out;
         }
-        ret = TRUE;
         break;
 
     default:
@@ -153,16 +189,28 @@ BOOL DG_CMN_DRV_SSD_get_serial(DG_CMN_DRV_SSD_ID_T ssd, char** str)
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_SSD_get_capacity(DG_CMN_DRV_SSD_ID_T ssd, DG_CMN_DRV_SSD_SIZE_T* size)
 {
-    BOOL ret         = FALSE;
-    int  size_fake[] = { 64000, 128000 };
+    BOOL  ret = FALSE;
+    char  shell_cmd[128];
+    char* p_out = NULL;
 
     switch (ssd)
     {
     case DG_CMN_DRV_SSD_OS:
     case DG_CMN_DRV_SSD_LOG:
-        *size = size_fake[ssd];
-        DG_DBG_TRACE("Get SSD device size 0x%02x", *size);
-        ret = TRUE;
+        sprintf(shell_cmd, "hdparm -I %s |grep 'device size with M = 1000' |cut -d: -f2",
+                dg_cmn_drv_device[ssd]);
+        if (DG_DRV_UTIL_system(shell_cmd, &p_out))
+        {
+            if (p_out != NULL)
+            {
+                if (sscanf(p_out, "%d", size) == 1)
+                {
+                    DG_DBG_TRACE("Get SSD device size %d MBytes", *size);
+                    ret = TRUE;
+                }
+                free(p_out);
+            }
+        }
         break;
 
     default:
@@ -180,15 +228,33 @@ BOOL DG_CMN_DRV_SSD_get_capacity(DG_CMN_DRV_SSD_ID_T ssd, DG_CMN_DRV_SSD_SIZE_T*
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_SSD_get_cache_size(DG_CMN_DRV_SSD_ID_T ssd, DG_CMN_DRV_SSD_SIZE_T* size)
 {
-    BOOL ret = FALSE;
+    BOOL  ret = FALSE;
+    char  shell_cmd[128];
+    char* p_out = NULL;
 
     switch (ssd)
     {
     case DG_CMN_DRV_SSD_OS:
     case DG_CMN_DRV_SSD_LOG:
-        *size = 16384;
-        DG_DBG_TRACE("Get SSD cache/buffer size 0x%02x", *size);
-        ret = TRUE;
+        sprintf(shell_cmd, "hdparm -I %s |grep 'buffer size' |cut -d: -f2",
+                dg_cmn_drv_device[ssd]);
+        if (DG_DRV_UTIL_system(shell_cmd, &p_out))
+        {
+            if (p_out != NULL)
+            {
+                if (sscanf(p_out, "%d", size) == 1)
+                {
+                    DG_DBG_TRACE("Get SSD cache/buffer size %d", *size);
+                }
+                else
+                {
+                    *size = 0;
+                    DG_DBG_TRACE("Get SSD cache/buffer size %d", *size);
+                }
+                ret = TRUE;
+                free(p_out);
+            }
+        }
         break;
 
     default:
