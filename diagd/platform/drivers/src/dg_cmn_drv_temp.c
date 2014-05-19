0a28331dc6ee8ff1f2faa871e1da231f94371f96
Xudong Huang <xudongh@juniper.net>
2014-05-19 15:06:55 +0800

    update psu temperature

--- diagd/platform/drivers/src/dg_cmn_drv_temp.c
+++ diagd/platform/drivers/src/dg_cmn_drv_temp.c
@@ -10,6 +10,9 @@
                                            INCLUDE FILES
 ==================================================================================================*/
 #include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+#include <errno.h>
 #include "dg_handler_inc.h"
 #include "dg_drv_util.h"
 #include "dg_cmn_drv_temp.h"
@@ -32,27 +35,53 @@ implementation of the Temperature driver
                                            LOCAL MACROS
 ==================================================================================================*/
 /* for chip TMP435 */
-#define TMP435_LOCAL_TEMP_HIGH_READ  0x00
-#define TMP435_REMOTE_TEMP_HIGH_READ 0x01
-#define TMP435_CONFIG_1_READ         0x03
-#define EXTENDED_RANGE_ENABLED       (1 << 2)
-#define TMP435_LOCAL_TEMP_LOW_READ   0x15
-#define TMP435_REMOTE_TEMP_LOW_READ  0x10
+#define TMP435_LOCAL_TEMP_HIGH_READ   0x00
+#define TMP435_REMOTE_TEMP_HIGH_READ  0x01
+#define TMP435_CONFIG_1_READ          0x03
+#define TMP435_EXTENDED_RANGE_ENABLED (1 << 2)
+#define TMP435_LOCAL_TEMP_LOW_READ    0x15
+#define TMP435_REMOTE_TEMP_LOW_READ   0x10
 
+/* for PSU temperature */
+#define PSMI_TEMPERATURE_REG 0x8e
+
+/* for CPU temperature */
+#define GET_CPU_CMD "cat /sys/module/coretemp/drivers/platform:coretemp/coretemp.0/temp1_input"
 
 /*==================================================================================================
                             LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
 ==================================================================================================*/
+typedef BOOL (* DG_CMN_DRV_TEMP_GET_T) (DG_CMN_DRV_I2C_BUS_T  bus,
+                                        DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                        float*                temp);
+
+typedef struct
+{
+    DG_CMN_DRV_I2C_BUS_T  bus;
+    DG_CMN_DRV_I2C_ADDR_T addr;
+    DG_CMN_DRV_TEMP_GET_T func;
+} DG_CMN_DRV_TEMP_INFO_T;
 
 /*==================================================================================================
                                      LOCAL FUNCTION PROTOTYPES
 ==================================================================================================*/
+extern int dg_cmn_drv_i2c_connect_device(DG_CMN_DRV_I2C_BUS_T bus, DG_CMN_DRV_I2C_ADDR_T address);
 
-static BOOL dg_cmn_drv_tmp435_get(DG_CMN_DRV_I2C_BUS_T  bus,
-                                  DG_CMN_DRV_I2C_ADDR_T dev_addr,
-                                  BOOL                  local,
-                                  float*                temp);
-static BOOL dg_cmn_drv_temp_cpu_get(float* temp);
+static BOOL dg_cmn_drv_temp_psu(DG_CMN_DRV_I2C_BUS_T  bus,
+                                DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                float*                temp);
+
+static BOOL dg_cmn_drv_temp_tmp435(DG_CMN_DRV_I2C_BUS_T  bus,
+                                   DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                   float*                temp);
+
+static BOOL dg_cmn_drv_temp_tmp435_local(DG_CMN_DRV_I2C_BUS_T  bus,
+                                         DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                         float*                temp);
+
+static BOOL dg_cmn_drv_temp_cpu(DG_CMN_DRV_I2C_BUS_T  bus,
+                                DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                float*                temp);
 
 /*==================================================================================================
                                          GLOBAL VARIABLES
@@ -61,6 +90,23 @@ static BOOL dg_cmn_drv_temp_cpu_get(float* temp);
 /*==================================================================================================
                                           LOCAL VARIABLES
 ==================================================================================================*/
+static DG_CMN_DRV_TEMP_INFO_T dg_cmn_drv_temp_info[] =
+{
+    [DG_CMN_DRV_TEMP_CB_0]  = { DG_CMN_DRV_I2C_MUX_CPU,  0x48, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_CB_1]  = { DG_CMN_DRV_I2C_MUX_CPU,  0x4e, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_FEB_0] = { DG_CMN_DRV_I2C_MUX_FEB,  0x4c, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_FEB_1] = { DG_CMN_DRV_I2C_MUX_FEB,  0x4e, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_FEB_2] = { DG_CMN_DRV_I2C_MUX_FEB,  0x4d, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_CPU]   = { DG_CMN_DRV_I2C_MUX_CPU,  0xff, dg_cmn_drv_temp_cpu },
+    [DG_CMN_DRV_TEMP_PCH]   = { DG_CMN_DRV_I2C_MUX_CPU,  0xff, NULL },
+    [DG_CMN_DRV_TEMP_PSU_0] = { DG_CMN_DRV_I2C_MUX_PSU0, 0x58, dg_cmn_drv_temp_psu },
+    [DG_CMN_DRV_TEMP_PSU_1] = { DG_CMN_DRV_I2C_MUX_PSU1, 0x58, dg_cmn_drv_temp_psu },
+    [DG_CMN_DRV_TEMP_WTB_1] = { DG_CMN_DRV_I2C_MUX_WTB1, 0x4c, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_WTB_2] = { DG_CMN_DRV_I2C_MUX_WTB2, 0x4c, dg_cmn_drv_temp_tmp435_local },
+    [DG_CMN_DRV_TEMP_PHY_0] = { DG_CMN_DRV_I2C_MUX_FEB,  0x4c, dg_cmn_drv_temp_tmp435 },
+    [DG_CMN_DRV_TEMP_PHY_1] = { DG_CMN_DRV_I2C_MUX_FEB,  0x4e, dg_cmn_drv_temp_tmp435 },
+    [DG_CMN_DRV_TEMP_FPGA]  = { DG_CMN_DRV_I2C_MUX_FEB,  0x4d, dg_cmn_drv_temp_tmp435 },
+};
 
 /*==================================================================================================
                                          GLOBAL FUNCTIONS
@@ -78,95 +124,28 @@ static BOOL dg_cmn_drv_temp_cpu_get(float* temp);
 *//*==============================================================================================*/
 BOOL DG_CMN_DRV_TEMP_get(DG_CMN_DRV_TEMP_SENSOR_T sensor, DG_CMN_DRV_TEMP_DATA_T* temperature)
 {
-    DG_CMN_DRV_I2C_BUS_T  bus;
-    DG_CMN_DRV_I2C_ADDR_T dev_addr;
-    BOOL                  local  = TRUE;
-    BOOL                  ret    = FALSE;
-    float                 f_temp = 23.4;
+    BOOL  ret    = FALSE;
+    float f_temp = 23.4;
 
-    switch (sensor)
+    if (sensor > DG_ARRAY_SIZE(dg_cmn_drv_temp_info))
     {
-    case DG_CMN_DRV_TEMP_CB_0:
-        /* intake temperature*/
-        bus      = DG_CMN_DRV_I2C_MUX_CPU;
-        dev_addr = 0x48;
-        break;
-
-    case DG_CMN_DRV_TEMP_CB_1:
-        /* exhaust temperature*/
-        bus      = DG_CMN_DRV_I2C_MUX_CPU;
-        dev_addr = 0x49;
-        break;
-
-    case DG_CMN_DRV_TEMP_FEB_0:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4c;
-        break;
-
-    case DG_CMN_DRV_TEMP_FEB_1:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4d;
-        break;
-
-    case DG_CMN_DRV_TEMP_FEB_2:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4e;
-        break;
-
-    case DG_CMN_DRV_TEMP_PHY_0:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4c;
-        local    = FALSE;
-        break;
-
-    case DG_CMN_DRV_TEMP_PHY_1:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4d;
-        local    = FALSE;
-        break;
-
-    case DG_CMN_DRV_TEMP_FPGA:
-        bus      = DG_CMN_DRV_I2C_MUX_FEB;
-        dev_addr = 0x4e;
-        local    = FALSE;
-        break;
-
-    case DG_CMN_DRV_TEMP_WTB_1:
-        bus      = DG_CMN_DRV_I2C_MUX_WTB1;
-        dev_addr = 0x4c;
-        break;
-
-    case DG_CMN_DRV_TEMP_WTB_2:
-        bus      = DG_CMN_DRV_I2C_MUX_WTB2;
-        dev_addr = 0x4c;
-        break;
-
-    case DG_CMN_DRV_TEMP_CPU:
-        break;
-
-    case DG_CMN_DRV_TEMP_PCH:
-    case DG_CMN_DRV_TEMP_PSU_0:
-    case DG_CMN_DRV_TEMP_PSU_1:
-        DG_DRV_UTIL_set_error_string("no such temperature sensor=%d", sensor);
-        return FALSE;
-
-    default:
         DG_DRV_UTIL_set_error_string("Invalid temperature sensor=%d", sensor);
         return FALSE;
     }
 
-    if (sensor != DG_CMN_DRV_TEMP_CPU)
+    if (dg_cmn_drv_temp_info[sensor].func == NULL)
     {
-        ret = dg_cmn_drv_tmp435_get(bus, dev_addr, local, &f_temp);
+        DG_DRV_UTIL_set_error_string("Unsupported temperature sensor=%d", sensor);
+        return FALSE;
     }
-    else
-    {
-        ret = dg_cmn_drv_temp_cpu_get(&f_temp);
-    }
-    if (ret == TRUE)
+
+    if (dg_cmn_drv_temp_info[sensor].func(dg_cmn_drv_temp_info[sensor].bus,
+                                          dg_cmn_drv_temp_info[sensor].addr,
+                                          &f_temp))
     {
         DG_DBG_TRACE("TEMP sensor %d got temperature: %+.2f C", sensor, f_temp);
         *temperature = *(UINT32*)&f_temp;
+        ret          = TRUE;
     }
 
     return ret;
@@ -175,68 +154,153 @@ BOOL DG_CMN_DRV_TEMP_get(DG_CMN_DRV_TEMP_SENSOR_T sensor, DG_CMN_DRV_TEMP_DATA_T
 /*==================================================================================================
                                           LOCAL FUNCTIONS
 ==================================================================================================*/
+
+/*=============================================================================================*//**
+@brief Get temperature from PSU PSMI
+
+@param[in]  bus      - chip bus number
+@param[in]  dev_addr - chip device address
+@param[out] temp     - The temperature from the sensor
+*//*==============================================================================================*/
+BOOL dg_cmn_drv_temp_psu(DG_CMN_DRV_I2C_BUS_T  bus,
+                         DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                         float*                temp)
+{
+    BOOL ret = FALSE;
+    int  fd;
+    int  r;
+
+    if ((fd = dg_cmn_drv_i2c_connect_device(bus, dev_addr)) < 0)
+    {
+        return FALSE;
+    }
+
+    if ((r = i2c_smbus_read_word_data(fd, PSMI_TEMPERATURE_REG)) < 0)
+    {
+        DG_DRV_UTIL_set_error_string("PSMI failed to read temperature, bus=%d, errno=%d(%m)",
+                                     bus, errno);
+    }
+    else
+    {
+        __s16 exponent;
+        __s32 mantissa;
+        long  val;
+
+        exponent = ((__s16)r) >> 11;
+        mantissa = ((__s16)((r & 0x7ff) << 5)) >> 5;
+
+        val = mantissa;
+
+        if (exponent >= 0)
+        {
+            val <<= exponent;
+        }
+        else
+        {
+            val >>= -exponent;
+        }
+
+        *temp = val;
+        ret   = TRUE;
+    }
+
+    close(fd);
+
+    return ret;
+}
+
 /*=============================================================================================*//**
 @brief Get temperature from TMP435 chip
 
 @param[in]  bus      - chip bus number
 @param[in]  dev_addr - chip device address
-@param[in]  local    - if TMP435 local sensor then true, else if remote then false.
 @param[out] temp     - The temperature from the sensor
 *//*==============================================================================================*/
-BOOL dg_cmn_drv_tmp435_get(DG_CMN_DRV_I2C_BUS_T  bus,
-                           DG_CMN_DRV_I2C_ADDR_T dev_addr,
-                           BOOL                  local,
-                           float*                temp)
+BOOL dg_cmn_drv_temp_tmp435(DG_CMN_DRV_I2C_BUS_T  bus,
+                            DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                            float*                temp)
 {
-    UINT8 config1, high, low, reg_low, reg_high;
+    BOOL  ret = FALSE;
+    UINT8 config1;
+    UINT8 high;
+    UINT8 low;
     float t;
 
-    if (local == TRUE)
-    {
-        reg_high = TMP435_LOCAL_TEMP_HIGH_READ;
-        reg_low  = TMP435_LOCAL_TEMP_LOW_READ;
-    }
-    else
-    {
-        reg_high = TMP435_REMOTE_TEMP_HIGH_READ;
-        reg_low  = TMP435_REMOTE_TEMP_LOW_READ;
-    }
-
     if (DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_CONFIG_1_READ, 1, &config1) &&
-        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, reg_high, 1, &high) &&
-        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, reg_low, 1, &low))
+        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_REMOTE_TEMP_HIGH_READ, 1, &high) &&
+        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_REMOTE_TEMP_LOW_READ, 1, &low))
     {
-
         t  = low >> 4;
         t *= 0.0625;
         t += high;
 
-        if (config1 & EXTENDED_RANGE_ENABLED)
+        if (config1 & TMP435_EXTENDED_RANGE_ENABLED)
         {
             t -= 64;
         }
         *temp = t;
-        return TRUE;
+        ret   = TRUE;
     }
 
-    return FALSE;
+    return ret;
 }
 
 /*=============================================================================================*//**
+@brief Get local temperature from TMP435 chip
+
+@param[in]  bus      - chip bus number
+@param[in]  dev_addr - chip device address
+@param[out] temp     - The temperature from the sensor
+*//*==============================================================================================*/
+BOOL dg_cmn_drv_temp_tmp435_local(DG_CMN_DRV_I2C_BUS_T  bus,
+                                  DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                                  float*                temp)
+{
+    BOOL  ret = FALSE;
+    UINT8 config1;
+    UINT8 high;
+    UINT8 low;
+    float t;
+
+    if (DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_CONFIG_1_READ, 1, &config1) &&
+        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_LOCAL_TEMP_HIGH_READ, 1, &high) &&
+        DG_CMN_DRV_I2C_read_bus(bus, dev_addr, TMP435_LOCAL_TEMP_LOW_READ, 1, &low))
+    {
+        t  = low >> 4;
+        t *= 0.0625;
+        t += high;
+
+        if (config1 & TMP435_EXTENDED_RANGE_ENABLED)
+        {
+            t -= 64;
+        }
+        *temp = t;
+        ret   = TRUE;
+    }
+
+    return ret;
+}
+/*=============================================================================================*//**
 @brief Get temperature from CPU
 
+@param[in]  bus      - NOT USED
+@param[in]  dev_addr - NOT USED
 @param[out] temp     - The temperature from the sensor
 @note
 - The command depends on the kernel module coretemp. So please confirm with "lsmod |grep coretemp".
 *//*==============================================================================================*/
-BOOL dg_cmn_drv_temp_cpu_get(float* temp)
+BOOL dg_cmn_drv_temp_cpu(DG_CMN_DRV_I2C_BUS_T  bus,
+                         DG_CMN_DRV_I2C_ADDR_T dev_addr,
+                         float*                temp)
 {
-    BOOL        ret       = FALSE;
-    const char* shell_cmd = "cat /sys/module/coretemp/drivers/platform:coretemp/coretemp.0/temp1_input";
-    char*       p_out     = NULL;
-    int         temp1;
+    BOOL  ret   = FALSE;
+    char* p_out = NULL;
+    int   temp1;
 
-    if (DG_DRV_UTIL_system(shell_cmd, &p_out))
+    DG_COMPILE_UNUSED(bus);
+    DG_COMPILE_UNUSED(dev_addr);
+
+    if (DG_DRV_UTIL_system(GET_CPU_CMD, &p_out))
     {
         if (p_out != NULL)
         {
